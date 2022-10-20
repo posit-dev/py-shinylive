f89ca9016b8f559371def6952375a5f4ce5b5bcf
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-20 12:52:48 +0000

    Adding tarfile member sanitization to extractall()

--- shinylive/_assets.py
+++ shinylive/_assets.py
@@ -33,7 +33,26 @@ def download_shinylive(
 
         print(f"Unzipping to {destdir}/", file=sys.stderr)
         with tarfile.open(tmp_name) as tar:
-            tar.extractall(destdir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, destdir)
     finally:
         if tmp_name is not None:
             Path(tmp_name).unlink(missing_ok=True)
