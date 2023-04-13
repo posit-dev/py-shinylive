76f5721f89cbd1604f4af5c717e343b0a7c2d35e
Winston Chang <winston@stdout.org>
2023-04-13 17:30:49 -0500

    Bump version to 0.0.10

--- shinylive/_version.py
+++ shinylive/_version.py
@@ -1,5 +1,5 @@
 # The version of this Python package.
-SHINYLIVE_PACKAGE_VERSION = "0.0.9.9000"
+SHINYLIVE_PACKAGE_VERSION = "0.0.10"
 
 # This is the version of the Shinylive assets to use.
 SHINYLIVE_ASSETS_VERSION = "0.0.13"
