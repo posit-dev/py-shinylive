c4477f414b0726a948019a758b2722387b1a0fc2
Barret Schloerke <barret@posit.co>
2023-10-02 12:45:39 -0400

    Rewrite `assets` to embrace `click` and use `groups` to define subcommands

--- shinylive/_main.py
+++ shinylive/_main.py
@@ -9,15 +9,36 @@ import click
 
 from . import _assets, _deps, _export, _version
 
+version_txt = f"""
+    \b
+    shinylive Python package version: {_version.SHINYLIVE_PACKAGE_VERSION}
+    shinylive web assets version:     {_assets.SHINYLIVE_ASSETS_VERSION}
+"""
+
+# CLI structure:
+# * shinylive
+#     * --version
+#     * export
+#         * Options: --verbose, --subdir, --full-shinylive
+#     * assets
+#         * download
+#             * Options: --version, --dir, --url
+#         * cleanup
+#             * Options: --dir
+#         * remove
+#             * Options: --version (required), --dir
+#         * info
+#             * Options: --dir
+#         * install-from-local
+#             * Options: --version, --dir, --source (required)
+#         * link-from-local
+#             * Options: --version, --dir, --source (required)
+#         * version
 
 @click.group(
     invoke_without_command=True,
     no_args_is_help=True,
-    help=f"""
-    \b
-    shinylive Python package version: {_version.SHINYLIVE_PACKAGE_VERSION}
-    shinylive web assets version:     {_assets.SHINYLIVE_ASSETS_VERSION}
-""",
+    help=version_txt,
 )
 @click.option(
     "--version",
@@ -29,6 +50,14 @@ from . import _assets, _deps, _export, _version
 def main(version: bool) -> None:
     if version:
         print(_version.SHINYLIVE_PACKAGE_VERSION)
+        # Quit early without error
+        # Prevents `shinylive --version assets` from executing `assets` command
+        click.get_current_context().exit(0)
+
+
+# #############################################################################
+# ## Export
+# #############################################################################
 
 
 @main.command(
@@ -87,93 +116,189 @@ def export(
     )
 
 
-@main.command(
+# #############################################################################
+# ## Assets
+# #############################################################################
+
+
+@main.group(
     help=f"""Manage local copy of Shinylive web assets
-
-    \b
-    shinylive Python package version: {_version.SHINYLIVE_PACKAGE_VERSION}
-    shinylive web assets version:     {_assets.SHINYLIVE_ASSETS_VERSION}
-
-    \b
-    Commands:
-      download: Download assets from the remote server.
-      cleanup: Remove all versions of local assets except the currently-used version, {_assets.SHINYLIVE_ASSETS_VERSION}.
-      remove: Remove a specific version of local copies of assets. Must be used with --version.
-      info: Print information about the local assets.
-      install-from-local: Install shinylive assets from a local directory. Must be used with --source.
-      link-from-local: Create a symlink to shinylive assets from a local shinylive build directory. Must be used with --source. This is useful when doing development on the Shinylive web assets.
-
+    {version_txt}
 """,
     no_args_is_help=True,
 )
-@click.argument("command", type=str)
+def assets() -> None:
+    pass
+
+
+def upgrade_dir(dir: str | Path | None) -> Path:
+    if dir is None:
+        dir = _assets.shinylive_cache_dir()
+    dir = Path(dir)
+    return dir
+
+
+@assets.command(help="""Download assets from the remote server.""")
 @click.option(
     "--version",
     type=str,
-    default=None,
-    help="Shinylive version to download or remove.",
-    show_default=True,
-)
-@click.option(
-    "--url",
-    type=str,
-    default=None,
-    help="URL to download from. If used, this will override --version.",
+    default=_version.SHINYLIVE_ASSETS_VERSION,
+    help="Shinylive version to download.",
     show_default=True,
 )
 @click.option(
     "--dir",
     type=str,
     default=None,
-    help="Directory to store shinylive assets (if not using the default)",
+    help="Directory to store shinylive assets (if not using the default).",
+)
+@click.option(
+    "--url",
+    type=str,
+    default=None,
+    help="URL to download from. If used, this will override --version.",
+)
+def download(
+    version: str,
+    dir: Optional[str | Path],
+    url: Optional[str],
+) -> None:
+    if version is None:  # pyright: ignore[reportUnnecessaryComparison]
+        version = _version.SHINYLIVE_ASSETS_VERSION
+    _assets.download_shinylive(destdir=upgrade_dir(dir), version=version, url=url)
+
+
+@assets.command(
+    help=f"Remove all versions of local assets except the currently-used version, {_assets.SHINYLIVE_ASSETS_VERSION}."
+)
+@click.option(
+    "--dir",
+    type=str,
+    default=None,
+    help="Directory where shinylive assets are stored (if not using the default).",
+)
+def cleanup(
+    dir: Optional[str | Path],
+) -> None:
+    _assets.cleanup_shinylive_assets(shinylive_dir=upgrade_dir(dir))
+
+
+@assets.command(
+    help="Remove a specific version of local copies of assets.",
+    no_args_is_help=True,
+)
+@click.option(
+    "--version",
+    type=str,
+    help="Shinylive version to remove.",
+    required=True,
+)
+@click.option(
+    "--dir",
+    type=str,
+    default=None,
+    help="Directory where shinylive assets are stored (if not using the default).",
+)
+def remove(
+    version: str,
+    dir: Optional[str | Path],
+) -> None:
+    if version is None:  # pyright: ignore[reportUnnecessaryComparison]
+        raise click.UsageError("Must specify --version")
+    dir = upgrade_dir(dir)
+    _assets.remove_shinylive_assets(shinylive_dir=dir, version=version)
+
+
+@assets.command(
+    name="info",
+    help="Print information about the local assets.",
+)
+@click.option(
+    "--dir",
+    type=str,
+    default=None,
+    help="Directory where shinylive assets are stored (if not using the default).",
+)
+def assets_info(
+    dir: Optional[str | Path],
+) -> None:
+    _assets.print_shinylive_local_info(destdir=upgrade_dir(dir))
+
+
+@assets.command(
+    help="Install shinylive assets from a local directory.",
+    no_args_is_help=True,
+)
+@click.option(
+    "--version",
+    type=str,
+    default=_version.SHINYLIVE_ASSETS_VERSION,
+    help="Version of the shinylive assets being copied.",
+    show_default=True,
+)
+@click.option(
+    "--dir",
+    type=str,
+    default=None,
+    help="Directory to store shinylive assets (if not using the default).",
 )
 @click.option(
     "--source",
     type=str,
     default=None,
-    help="Directory where shinylive assets will be copied from. Must be used with 'copy' command.",
+    help="Directory where shinylive assets will be copied from.",
+    required=True,
 )
-def assets(
-    command: str,
-    version: Optional[str],
-    url: Optional[str],
+def install_from_local(
+    source: str,
+    version: str,
     dir: Optional[str | Path],
-    source: Optional[str],
 ) -> None:
-    if dir is None:
-        dir = _assets.shinylive_cache_dir()
-    dir = Path(dir)
+    if source is None:  # pyright: ignore[reportUnnecessaryComparison]
+        raise click.UsageError("Must specify --source")
+    dir = upgrade_dir(dir)
+    if version is None:  # pyright: ignore[reportUnnecessaryComparison]
+        version = _version.SHINYLIVE_ASSETS_VERSION
+    print(f"Copying shinylive-{version} from {source} to {dir}")
+    _assets.copy_shinylive_local(source_dir=source, destdir=dir, version=version)
 
-    if command == "download":
-        if version is None:
-            version = _version.SHINYLIVE_ASSETS_VERSION
-        _assets.download_shinylive(destdir=dir, version=version, url=url)
-    elif command == "cleanup":
-        _assets.cleanup_shinylive_assets(shinylive_dir=dir)
-    elif command == "remove":
-        if version is None:
-            raise click.UsageError("Must specify --version")
-        _assets.remove_shinylive_assets(shinylive_dir=dir, version=version)
-    elif command == "info":
-        _assets.print_shinylive_local_info()
-    elif command == "install-from-local":
-        if source is None:
-            raise click.UsageError("Must specify --source")
-        if version is None:
-            version = _version.SHINYLIVE_ASSETS_VERSION
-        print(f"Copying shinylive-{version} from {source} to {dir}")
-        _assets.copy_shinylive_local(source_dir=source, destdir=dir, version=version)
-    elif command == "link-from-local":
-        if source is None:
-            raise click.UsageError("Must specify --source")
-        if version is None:
-            version = _version.SHINYLIVE_ASSETS_VERSION
-        print(f"Creating symlink for shinylive-{version} from {source} to {dir}")
-        _assets.link_shinylive_local(source_dir=source, destdir=dir, version=version)
-    elif command == "version":
-        print(_assets.SHINYLIVE_ASSETS_VERSION)
-    else:
-        raise click.UsageError(f"Unknown command: {command}")
+
+@assets.command(
+    help="""Create a symlink to shinylive assets from a local shinylive build directory.""",
+    no_args_is_help=True,
+)
+@click.option(
+    "--version",
+    type=str,
+    default=_version.SHINYLIVE_ASSETS_VERSION,
+    help="Version of shinylive assets being linked.",
+    show_default=True,
+)
+@click.option(
+    "--dir",
+    type=str,
+    default=None,
+    help="Directory to store shinylive assets (if not using the default).",
+)
+@click.option(
+    "--source",
+    type=str,
+    default=None,
+    help="Directory where shinylive assets will be linked from.",
+    required=True,
+)
+def link_from_local(
+    source: str,
+    version: str,
+    dir: Optional[str | Path],
+) -> None:
+    if source is None:  # pyright: ignore[reportUnnecessaryComparison]
+        raise click.UsageError("Must specify --source")
+    dir = upgrade_dir(dir)
+    if version is None:  # pyright: ignore[reportUnnecessaryComparison]
+        version = _version.SHINYLIVE_ASSETS_VERSION
+    print(f"Creating symlink for shinylive-{version} from {source} to {dir}")
+    _assets.link_shinylive_local(source_dir=source, destdir=dir, version=version)
 
 
 @main.command(
