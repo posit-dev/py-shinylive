d570bfec6468796d7dcdfb40557ae0b02604a8ba
Garrick Aden-Buie <garrick@adenbuie.com>
2024-01-04 14:18:58 -0500

    export `make_shinylive_url()`

--- shinylive/__init__.py
+++ shinylive/__init__.py
@@ -1,5 +1,8 @@
 """A package for packaging Shiny applications that run on Python in the browser."""
 
 from . import _version
+from ._url import make_shinylive_url
 
 __version__ = _version.SHINYLIVE_PACKAGE_VERSION
+
+__all__ = ("make_shinylive_url",)
