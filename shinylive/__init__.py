18cf274df4db97e331cc4c8bc44d945e7f0e6bdd
Garrick Aden-Buie <garrick@adenbuie.com>
2024-01-04 15:59:14 -0500

    noqa and pyright ingore

--- shinylive/__init__.py
+++ shinylive/__init__.py
@@ -1,6 +1,6 @@
 """A package for packaging Shiny applications that run on Python in the browser."""
 
 from . import _version
-from ._url import make_shinylive_url
+from ._url import make_shinylive_url  # noqa # pyright: ignore
 
 __version__ = _version.SHINYLIVE_PACKAGE_VERSION
