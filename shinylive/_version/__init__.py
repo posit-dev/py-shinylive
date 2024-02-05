e14ea9889df31ef11a8c717dd3fe3566c4b39a0d
Winston Chang <winston@posit.co>
2024-02-05 14:52:31 -0600

    Bump version to 0.2.2, shinylive assets to 0.2.6

--- shinylive/_version/__init__.py
+++ shinylive/_version/__init__.py
@@ -1,5 +1,5 @@
 # The version of this Python package.
-SHINYLIVE_PACKAGE_VERSION = "0.2.1"
+SHINYLIVE_PACKAGE_VERSION = "0.2.2"
 
 # This is the version of the Shinylive assets to use.
-SHINYLIVE_ASSETS_VERSION = "0.2.5"
+SHINYLIVE_ASSETS_VERSION = "0.2.6"
