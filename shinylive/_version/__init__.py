d2f5c0a0e3e49cfa55efbc391e94e1b590e8ccc3
Winston Chang <winston@posit.co>
2024-07-24 15:13:04 -0500

    Bump version to 0.5.0.9000

--- shinylive/_version/__init__.py
+++ shinylive/_version/__init__.py
@@ -1,5 +1,5 @@
 # The version of this Python package.
-SHINYLIVE_PACKAGE_VERSION = "0.5.0"
+SHINYLIVE_PACKAGE_VERSION = "0.5.0.9000"
 
 # This is the version of the Shinylive assets to use.
 SHINYLIVE_ASSETS_VERSION = "0.5.0"
