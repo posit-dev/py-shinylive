20073e53c3eddae33b12a69f0eedc66a7f59efe9
Carson <cpsievert1@gmail.com>
2024-09-03 15:57:23 -0500

    Use decorator to skip pytest tests (and ensure assets before running tests that need them)

--- tests/test_assets.py
+++ tests/test_assets.py
@@ -6,9 +6,17 @@ import pytest
 
 import shinylive._assets
 
-
 # Don't run this test in CI, unless we're triggered by a release event. In the future,
 # it would make sense to run this test when we're on an rc branch.
+skip_if_not_release = (
+    os.environ.get("CI") == "true" and os.environ.get("GITHUB_EVENT_NAME") != "release"
+)
+
+
+@pytest.mark.skipif(
+    skip_if_not_release,
+    reason="Don't run this test in CI, unless we're on a release branch.",
+)
 def test_assets_available():
     """
     Test that the assets are available at the remote URL. Although this test might not
