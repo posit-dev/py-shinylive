ceaa1496a735663743b72ac2c36d18391429b277
Carson <cpsievert1@gmail.com>
2024-12-18 11:22:55 -0600

    v0.7.3

--- shinylive/_version/__init__.py
+++ shinylive/_version/__init__.py
@@ -1,5 +1,5 @@
 # The version of this Python package.
-SHINYLIVE_PACKAGE_VERSION = "0.7.2"
+SHINYLIVE_PACKAGE_VERSION = "0.7.3"
 
 # This is the version of the Shinylive assets to use.
-SHINYLIVE_ASSETS_VERSION = "0.9.2"
+SHINYLIVE_ASSETS_VERSION = "0.9.3"
