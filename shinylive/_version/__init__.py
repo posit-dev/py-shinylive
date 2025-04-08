b4854f095f6eb43551075e315e17b9b03a65d375
Barret Schloerke <barret@posit.co>
2025-04-08 12:56:24 -0400

    Update assets version: 0.10.4; py-shinylive version: 0.8.3

--- shinylive/_version/__init__.py
+++ shinylive/_version/__init__.py
@@ -1,5 +1,5 @@
 # The version of this Python package.
-SHINYLIVE_PACKAGE_VERSION = "0.8.2"
+SHINYLIVE_PACKAGE_VERSION = "0.8.3"
 
 # This is the version of the Shinylive assets to use.
-SHINYLIVE_ASSETS_VERSION = "0.10.3"
+SHINYLIVE_ASSETS_VERSION = "0.10.4"
