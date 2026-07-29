73940c0afa8bcbd35f37989dd6bea49743f620ae
Barret Schloerke <barret@posit.co>
2026-07-29 01:18:07 -0400

    release: v0.8.11
    Update to Shinylive web assets 0.10.14, which bundles Shiny for Python 1.7.0
    (and shinyswatch 0.12.0).

--- shinylive/_version/__init__.py
+++ shinylive/_version/__init__.py
@@ -1,5 +1,5 @@
 # The version of this Python package.
-SHINYLIVE_PACKAGE_VERSION = "0.8.10"
+SHINYLIVE_PACKAGE_VERSION = "0.8.11"
 
 # This is the version of the Shinylive assets to use.
-SHINYLIVE_ASSETS_VERSION = "0.10.13"
+SHINYLIVE_ASSETS_VERSION = "0.10.14"
